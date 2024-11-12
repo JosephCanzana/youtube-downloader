989f28a53c7a48864cc7377a69a2abb85053be22
JosephCanzana <josephcanzana8@gmail.com>
2024-11-12 21:41:46 +0800

    Prototype, all necessary code files is working
    Added app.py #but only printing if succes not actually downloading it
    Added layout for html
    Added success, apology, and index html
    Added css sheet

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from pytubefix import YouTube
+from flask import Flask, flash, redirect, render_template, request
 
+app = Flask(__name__)
 
-yt = YouTube('https://youtu.be/dQw4w9WgXcQ?si=dKMIypGK_maFXuG5')
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        url = request.form.get("url")
+        try:
+            yt = YouTube(url)
+            title = yt.title
+            #yt.streams.get_highest_resolution().download(output_path='./downloads')
+        except:
+            return apology("Invalid", 400)   
 
-print(yt.title)
-#yt.streams.get_highest_resolution().download()
+        return render_template("success.html", title=title)
+    else:
+        return render_template("index.html") 
+    
+def apology(message, code):
+    return render_template("apology.html", message=message, code=code)
