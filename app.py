fc6decfb7e6b0b4311a258bb144dca25fe18c992
JosephCanzana <josephcanzana8@gmail.com>
2024-11-14 16:12:51 +0800

    Fix bug, now allows local user to save yt vid locally
    but the success template not displaying

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
+import os
+
 from pytubefix import YouTube
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, send_file
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
+
         url = request.form.get("url")
+
         try:
             yt = YouTube(url)
             title = yt.title
-            yt.streams.get_highest_resolution().download(output_path='./downloads')
+            file_path = yt.streams.get_highest_resolution().download(output_path='./downloads')
+            file = send_file(file_path, as_attachment=True, download_name=f"{title}.mp4")
+            
+            if os.path.exists(file_path):
+                os.remove(file_path)
+            return file 
         except:
-            return apology("Invalid URL", 400)   
-
-        return render_template("success.html", title=title)
+            return apology("Invalid URL", 400) 
     else:
         return render_template("index.html")
+
     
 def apology(message, code):
     return render_template("apology.html", message=message, code=code)
