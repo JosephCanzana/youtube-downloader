df6dbfa55b08303cafe4f6f90e01443ed22f386c
JosephCanzana <josephcanzana8@gmail.com>
2024-11-17 08:39:47 +0800

    adding vercer.json

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def apology(message, code):
     return render_template("apology.html", message=message, code=code)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
